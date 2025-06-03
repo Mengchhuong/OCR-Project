c431cb400855c4f6ccef545ee945bca8daa8effa
net-specter <kaovichet11@gmail.com>
2025-06-03 16:26:55 +0700

    create component toogle

---
+++ frontend/components/toggles/LanguageToggle.tsx
@@ -0,0 +1,21 @@
+"use client";
+import React, { useState } from "react";
+import "../../app/layout";
+import { cadtMonoDisplay } from "../../app/fonts/fonts";
+
+export default function LanguageToggle() {
+  const [active, setActive] = useState("en");
+
+  const toggleLanguage = () => {
+    setActive((prev) => (prev === "en" ? "km" : "en"));
+  };
+
+  return (
+    <button
+      className={`hover:text-[#FF4438] duration-300 cursor-pointer font-bold px-2 py-1 rounded`}
+      onClick={toggleLanguage}
+    >
+      {active === "en" ? "English" : "Khmer"}
+    </button>
+  );
+}
---
+++ frontend/components/toggles/ModeToggle.tsx
@@ -0,0 +1,31 @@
+"use client";
+import * as React from "react";
+import { MoonIcon, SunIcon } from "lucide-react";
+import { useTheme } from "next-themes";
+import { Button } from "@/components/ui/button";
+
+export default function ModeToggle() {
+  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = React.useState(false);
+
+  React.useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
+  return (
+    <div>
+      <Button
+        variant="ghost"
+        size="icon"
+        className="cursor-pointer hover:text-[#FF4438] duration-300 relative h-10 w-10 rounded-full p-0 transition-colors hover:bg-accent focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent"
+        onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+      >
+        <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-150 transition-all dark:-rotate-90 dark:scale-0" />
+        <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-150" />
+        <span className="sr-only">Toggle theme</span>
+      </Button>
+    </div>
+  );
+}
